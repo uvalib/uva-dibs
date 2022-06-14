a3fdb9517b0a1a19fb9aac85091ef59ff69cdc8c
Dave Goldstein <dpg3k@virginia.edu>
2022-06-14 13:38:18 -0400

    Really really send those logs to stdout/stderr

--- package/scripts/entry.sh
+++ package/scripts/entry.sh
@@ -8,6 +8,10 @@ render_template() {
 # generate the settings file from the template
 render_template settings.ini.template > settings.ini
 
+# redirect to standout/stderr
+ln -sf /proc/$$/fd/1 /var/log/apache2/access.log
+ln -sf /proc/$$/fd/2 /var/log/apache2/error.log
+
 # start Apache
 /usr/sbin/apache2ctl -DFOREGROUND
 
