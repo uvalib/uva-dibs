a04a4072f029f61e077adced498ebfa35826c312
Dave Goldstein <dpg3k@virginia.edu>
2022-06-16 08:48:24 -0400

    Add Mike D and Mark W as admins

--- package/scripts/add-admins.sh
+++ package/scripts/add-admins.sh
@@ -2,9 +2,11 @@
 
 # add the admins
 admin/people-manager add role="library" uname="dpg3k"   # Dave Goldstein
+admin/people-manager add role="library" uname="jlk4p"   # Jack Kelly
+admin/people-manager add role="library" uname="md5wz"   # Mike Durbin
+admin/people-manager add role="library" uname="mhw8m"   # Mark Witteman
 admin/people-manager add role="library" uname="rar6u"   # Renee Reighart
 admin/people-manager add role="library" uname="rmg6f"   # Bob Gartland
-admin/people-manager add role="library" uname="jlk4p"   # Jack Kelly
 
 # ensure permissions
 admin/set-server-permissions --owner www-data --group www-data
