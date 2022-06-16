13fc3fd7c5bb585d926d4ac47a3f59b509c71995
Dave Goldstein <dpg3k@virginia.edu>
2022-06-16 07:47:24 -0400

    Add Renee R as an admin

--- package/scripts/add-admins.sh
+++ package/scripts/add-admins.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 # add the admins
-admin/people-manager add role="library" uname="dpg3k"
-admin/people-manager add role="library" uname="rmg6f"
-admin/people-manager add role="library" uname="jlk4p"
+admin/people-manager add role="library" uname="dpg3k"   # Dave Goldstein
+admin/people-manager add role="library" uname="rar6u"   # Renee Reighart
+admin/people-manager add role="library" uname="rmg6f"   # Bob Gartland
+admin/people-manager add role="library" uname="jlk4p"   # Jack Kelly
 
 # ensure permissions
 admin/set-server-permissions --owner www-data --group www-data
